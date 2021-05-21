b3c4c6c0156de338debe462ec5d0e34b4221c5c9
dell <dell@DESKTOP-SAL80FM>
2021-05-21 13:19:10 +0530

    ghfgh

--- OnlineshopApplication/src/main/java/com/example/onlinesportshopee/entities/CustomerEntity.java
+++ OnlineshopApplication/src/main/java/com/example/onlinesportshopee/entities/CustomerEntity.java
@@ -15,7 +15,6 @@ public class CustomerEntity
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	
 	private Long id;
 	@Column(name = "name")
 	private String name;
---
+++ OnlineshopApplication/src/test/java/com/example/OnlineSportsShopee/OrderServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.example.OnlineSportsShopee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.onlinesportshopee.entities.CustomerEntity;
+import com.example.onlinesportshopee.entities.PaymentEntity;
+import com.example.onlinesportshopee.entities.ProductEntity;
+import com.example.onlinesportshopee.exception.InvalidOrderIdException;
+import com.example.onlinesportshopee.exception.OrderNotFoundException;
+import com.example.onlinesportshopee.services.IOrderService;
+
+@SpringBootTest
+class OrderServiceImplTest {
+
+	final static Logger LOGGER = LoggerFactory.getLogger(OrderServiceImplTest.class);
+
+	@Autowired
+	IOrderService service;
+	
+	CustomerEntity custEnt;
+	ProductEntity proEnt;
+	PaymentEntity payEnt;
+	
+	@BeforeAll
+	public static void init() {
+		LOGGER.info("Order Testing Initiated");
+	}
+	
+	@Test
+	void testAddOrder1()throws OrderNotFoundException,InvalidOrderIdException{
+		custEnt=new CustomerEntity(1,"Rama","9873562781",LocalDate.parse("1995-09-30"),)
+	}
+
+}
