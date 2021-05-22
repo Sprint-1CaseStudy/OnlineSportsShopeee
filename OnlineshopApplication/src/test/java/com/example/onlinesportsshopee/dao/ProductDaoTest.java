e1ed25e1a9e4e5fca48f67b5b31f67b0936f7d50
azeem <azeem@Asus-Vivobook>
2021-05-22 18:27:16 +0530

    productdao and contoller test classes

---
+++ OnlineshopApplication/src/test/java/com/example/onlinesportsshopee/dao/ProductDaoTest.java
@@ -0,0 +1,14 @@
+package com.example.onlinesportsshopee.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductDaoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
