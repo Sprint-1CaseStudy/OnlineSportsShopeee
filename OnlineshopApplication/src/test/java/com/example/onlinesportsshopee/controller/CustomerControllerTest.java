814fde40b96552747c8654a27353a563929729f1
HP <HP@LAPTOP-QQDE9910>
2021-05-22 19:59:38 +0530

    ...

--- OnlineshopApplication/src/test/java/com/example/onlinesportsshopee/controller/CustomerControllerTest.java
+++ OnlineshopApplication/src/test/java/com/example/onlinesportsshopee/controller/CustomerControllerTest.java
@@ -1,11 +1,11 @@
 package com.example.onlinesportsshopee.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import com.example.onlinesportshopee.controller.CustomerController;
@@ -22,6 +22,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
